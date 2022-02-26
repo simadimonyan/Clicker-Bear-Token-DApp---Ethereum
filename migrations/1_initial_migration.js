a297388540ac34c5fa48eb4e4a0e0691163e7501
Dima Simonyan <simadimonyan@gmail.com>
2022-02-26 17:39:51 +0300

    Truffle Deploy

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,18 +1,26 @@
 const wallet = artifacts.require("MultiSigBearWallet");
 const channel = artifacts.require("PaymentChannel");
 const token = artifacts.require("Token");
-let owners = ['0xC2E06Dd93a8edA1c363C5656b85f2dB52c7eB927', '0x9e2addbfC16eEB1F7e46A38580e7bC05fA38CeD4'];
-let multiSigWalletAddress = "0x424dABAd4ad8dE3C6d7B1CC9Aa3d88a152ff1b3C";
-let channelAddress = "0x3d50d99F93e7e7d28778F78ab4763d6955048ceB";
+let owners = ['0x29b565535918d954f7a1d6c5525d5c8b022f4f0b', '0xbddef9f6288b774ffdc41becd0296f70e9c518bd'];
+let multiSigWalletAddress;
+let channelAddress;
 
 module.exports = function (deployer) {
-  // deployer.deploy(wallet,
-  //   owners,
-  //   2);
-  // deployer.deploy(channel,
-  //   multiSigWalletAddress);
-  // deployer.deploy(token,
-  //   multiSigWalletAddress,
-  //   channelAddress,
-  //   owners);
+  deployer.deploy(wallet,
+    owners,
+    2).then( async () => {
+      multiSigWalletAddress = wallet.address;
+      console.log(multiSigWalletAddress);
+    }).then( async () => {
+      await deployer.deploy(channel,
+        multiSigWalletAddress);
+    }).then( async () => {
+      channelAddress = channel.address;
+      console.log(channelAddress);
+    }).then( async () => {
+      await deployer.deploy(token,
+        multiSigWalletAddress,
+        channelAddress,
+        owners);
+    });
 };
